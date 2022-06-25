86a35761be5d326fbefcdc8d1c32d2163271c427
Jihogrammer <jiho@kakao.com>
2022-06-25 15:29:31 +0900

    ✏️ about pandas features

---
+++ initializer.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from IPython.core.interactiveshell import InteractiveShell
+
+def init() -> None:
+    InteractiveShell.ast_node_interactivity = "all"
+    pd.set_option('display.float_format', lambda x: '%.3f' % x)
+    pd.options.display.max_columns = None
+    np.random.seed(1)
